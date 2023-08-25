3a9981e3dcb1ab56ee0d6d673914742241b33561
Steven7981 <stevenfengwu79@gmail.com>
2023-08-25 16:16:52 -0600

    Create listadobleenlazada.h

---
+++ listadobleenlazada.h
@@ -0,0 +1,20 @@
+// Steven Feng Wu - 2023083127
+
+    /*
+    Lista elegida:
+        Una lista doble enlazada
+*/
+    
+/*
+    Metodos a usar:
+        - insertarPosicion(noticia, posicion):
+          Recibe una noticia y una posicion, inserta la noticia en la posicion recibida
+
+        - agregarNoticia(noticia):
+          Agrega una noticia al final de la lista
+
+        - eliminarNoticia(noticia, posicion):
+          Elimina la noticia de la posicion recibida
+
+        - 
+        */
