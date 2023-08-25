417edf88dbd28f6dd1290f4bf8f890f5ae94ac32
Steven7981 <stevenfengwu79@gmail.com>
2023-08-25 17:10:32 -0600

    Update news.h

--- news.h
+++ news.h
@@ -1,1 +1,22 @@
 //Steven Feng Wu - 2023083127
+
+/*
+    A. showTop5():
+        mostrarNoticia(int posicion):
+        - Se pone un contador empezando desde 0 hasta 4, mostrando los primeros 5
+
+    B. showAll():
+        mostrarNoticias(string palabra):
+        - Muestra todas las noticias con la palabra recibida
+    
+    C. eliminarNoticia()
+        borrarNoticias(string palabra)
+    
+    D. ordenarNoticias(posicion, posicion_mover):
+        buscarNoticia(int posicion):
+        borrarNoticia(int posicion):
+        borrarNoticia(int (posicion + posicion_mover))
+        insertarNoticia(noticia,int posicion)
+
+        
+*/      
