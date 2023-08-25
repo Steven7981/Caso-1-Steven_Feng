376f30bf303234ed22a2c931f0b739bcf2d46062
Steven7981 <stevenfengwu79@gmail.com>
2023-08-25 17:13:03 -0600

    Update news.h

--- news.h
+++ news.h
@@ -11,12 +11,16 @@
     
     C. eliminarNoticia()
         borrarNoticias(string palabra)
+        - Borra todas las noticias que contenga la palabra recibida en el titulo
     
     D. ordenarNoticias(posicion, posicion_mover):
         buscarNoticia(int posicion):
         borrarNoticia(int posicion):
         borrarNoticia(int (posicion + posicion_mover))
         insertarNoticia(noticia,int posicion)
+        - Busca la noticia que quiere mover, la elimina y luego elimina la noticia de la 
+        posicion a la que quiere ubicar, inserta la noticia que quiere mover a la posicion 
+        deseada
 
         
 */      
