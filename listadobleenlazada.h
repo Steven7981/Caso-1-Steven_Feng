dacfe54f054407e0ae6dcbe3ec6c74eace500e29
Steven7981 <stevenfengwu79@gmail.com>
2023-08-25 16:30:04 -0600

    Update listadobleenlazada.h

--- listadobleenlazada.h
+++ listadobleenlazada.h
@@ -13,8 +13,20 @@
         - agregarNoticia(noticia):
           Agrega una noticia al final de la lista
 
-        - eliminarNoticia(noticia, posicion):
+        - eliminarNoticia(int posicion):
           Elimina la noticia de la posicion recibida
 
-        - 
+        - eliminar(int noticia):
+          Elimina la ultima noticia de la lista
+        
+        - mostrarNoticia(int posicion):
+          Recibe una posicion, y al encontrar la noticia, se muestra
+        
+        - mostrarNoticias(string palabra)
+          Esta muestra todas las noticias que tenga la palabra ingresada
+
+        - buscar(string palabra):
+          Busca las noticias que contenga la palabra recibida
+        
+
         */
